c47318d1df5c2ac97b455219207a5d3c9cfd2d0d
Alexandre Barreiro Neto <alexandrebn01@gmail.com>
2020-11-07 23:44:17 -0300

    07-refactoring-testing: add postman and profile

--- commons/src/main/java/br/com/votingsessionmanager/commons/infrastructure/handler/ErrorCustom.java
+++ commons/src/main/java/br/com/votingsessionmanager/commons/infrastructure/handler/ErrorCustom.java
@@ -3,6 +3,8 @@ package br.com.votingsessionmanager.commons.infrastructure.handler;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 /**
  * The class {@code ErrorCustom} is used to return errors
  */
@@ -17,6 +19,7 @@ public class ErrorCustom {
 		return errors;
 	}
 
+	@JsonIgnore
 	public Set<String> getErrorMessages() {
 		return errors.stream().map(FieldErrorCustom::getErrorDescription).collect(Collectors.toSet());
 	}
